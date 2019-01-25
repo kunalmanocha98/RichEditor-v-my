7dd569cb3afbe7991afe6ff689fcae3eb10cc0a6
kunal <kunal@agnitioworld.com>
2019-01-25 15:05:17 +0530

    first commit

--- sample/src/main/java/jp/wasabeef/sample/MainActivity.java
+++ sample/src/main/java/jp/wasabeef/sample/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
   private RichEditor mEditor;
   private TextView mPreview;
 
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
